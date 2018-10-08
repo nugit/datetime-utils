ea72f697a35fe25db2bebe04b445cd3f470d4e66
Bertrand31 <bertrandjun@gmail.com>
2018-10-08 16:59:06 +0800

    99.42% coverage

--- test/retrieve_comparison__test.js
+++ test/retrieve_comparison__test.js
@@ -39,6 +39,12 @@ describe('retrieve period string', () => {
     expect(period.end).to.equal('2017-03-19');
   });
 
+  it('retrieve auto comparison for this week, implicitly using auto mode', () => {
+    const period = datetimeUtils.retrieveComparePeriod('this_week');
+    expect(period.start).to.equal('2017-03-13');
+    expect(period.end).to.equal('2017-03-19');
+  });
+
   it('retrieve auto comparison for last week', () => {
     const period = datetimeUtils.retrieveComparePeriod('last_week', 'auto');
     expect(period.start).to.equal('2017-03-06');
@@ -57,13 +63,13 @@ describe('retrieve period string', () => {
     expect(period.end).to.equal('2016-12-31');
   });
 
-  it('retrieve auto comparison for custom period (fit whole month)', () => {
+  it('retrieve auto comparison for custom period (fit the whole month)', () => {
     const period = datetimeUtils.retrieveComparePeriod({ start: '2017-06-01', end: '2017-06-30' }, 'auto');
     expect(period.start).to.equal('2017-05-01');
     expect(period.end).to.equal('2017-05-31');
   });
 
-  it('retrieve auto comparison for custom period(fit whole year)', () => {
+  it('retrieve auto comparison for custom period (fit the whole year)', () => {
     const period = datetimeUtils.retrieveComparePeriod({ start: '2016-01-01', end: '2016-12-31' }, 'auto');
     expect(period.start).to.equal('2015-01-01');
     expect(period.end).to.equal('2015-12-31');
