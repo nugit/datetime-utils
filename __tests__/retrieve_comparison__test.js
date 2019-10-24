b1c66738a075ef6419e9aac3a5ba778e853b5337
Lim Ting Zhi <limtingzhi@gmail.com>
2019-10-24 20:32:48 +0800

    Change to named exports instead of default exports

--- __tests__/retrieve_comparison__test.js
+++ __tests__/retrieve_comparison__test.js
@@ -1,101 +1,102 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import datetimeUtils from '../src/index';
+import { retrieveComparePeriod } from '../src/index';
 
 describe('#retrieveComparePeriod', () => {
   describe('when current date is Mon, 20 Mar 2017', () => {
     let clock;
+
     beforeAll(() => {
       clock = sinon.useFakeTimers(new Date('2017-03-20').getTime());
     });
 
     it('should auto compare period for today be the day before yesterday (today is deprecated and replaced by yesterday)', () => {
-      const period = datetimeUtils.retrieveComparePeriod('today', 'auto');
+      const period = retrieveComparePeriod('today', 'auto');
       expect(period.start).to.equal('2017-03-18');
       expect(period.end).to.equal('2017-03-18');
     });
 
     it('should auto compare period for yesterday be the day before', () => {
-      const period = datetimeUtils.retrieveComparePeriod('yesterday', 'auto');
+      const period = retrieveComparePeriod('yesterday', 'auto');
       expect(period.start).to.equal('2017-03-18');
       expect(period.end).to.equal('2017-03-18');
     });
 
     it('should auto compare period for last week be two weeks before', () => {
-      const period = datetimeUtils.retrieveComparePeriod('last_week', 'auto');
+      const period = retrieveComparePeriod('last_week', 'auto');
       expect(period.start).to.equal('2017-03-06');
       expect(period.end).to.equal('2017-03-12');
     });
 
     it('should auto compare period for this week be two weeks before as today is Monday', () => {
-      const period = datetimeUtils.retrieveComparePeriod('this_week', 'auto');
+      const period = retrieveComparePeriod('this_week', 'auto');
       expect(period.start).to.equal('2017-03-06');
       expect(period.end).to.equal('2017-03-12');
     });
 
     it('should auto compare period for last week be two weeks before, implicitly using auto mode', () => {
-      const period = datetimeUtils.retrieveComparePeriod('last_week');
+      const period = retrieveComparePeriod('last_week');
       expect(period.start).to.equal('2017-03-06');
       expect(period.end).to.equal('2017-03-12');
     });
 
     it('should auto compare period for this month be last month', () => {
-      const period = datetimeUtils.retrieveComparePeriod('this_month', 'auto');
+      const period = retrieveComparePeriod('this_month', 'auto');
       expect(period.start).to.equal('2017-02-01');
       expect(period.end).to.equal('2017-02-28');
     });
 
     it('should auto compare period for year_to_date be last year', () => {
-      const period = datetimeUtils.retrieveComparePeriod('year_to_date', 'auto');
+      const period = retrieveComparePeriod('year_to_date', 'auto');
       expect(period.start).to.equal('2016-01-01');
       expect(period.end).to.equal('2016-12-31');
     });
 
     it('should retrieve auto compare period be previous period of same duration', () => {
-      const period = datetimeUtils.retrieveComparePeriod({ start: '2017-03-15', end: '2017-03-19' }, 'auto');
+      const period = retrieveComparePeriod({ start: '2017-03-15', end: '2017-03-19' }, 'auto');
       expect(period.start).to.equal('2017-03-10');
       expect(period.end).to.equal('2017-03-14');
     });
 
     it('should auto compare period for this quarter be last quarter', () => {
-      const period = datetimeUtils.retrieveComparePeriod('this_quarter', 'auto');
+      const period = retrieveComparePeriod('this_quarter', 'auto');
       expect(period.start).to.equal('2016-10-01');
       expect(period.end).to.equal('2016-12-31');
     });
 
     it('should auto compare period for custom period fitting the whole month be the month before ', () => {
-      const period = datetimeUtils.retrieveComparePeriod({ start: '2017-06-01', end: '2017-06-30' }, 'auto');
+      const period = retrieveComparePeriod({ start: '2017-06-01', end: '2017-06-30' }, 'auto');
       expect(period.start).to.equal('2017-05-01');
       expect(period.end).to.equal('2017-05-31');
     });
 
     it('should auto compare period not fitting the whole month be a period of the same duration', () => {
-      const period = datetimeUtils.retrieveComparePeriod({ start: '2017-06-01', end: '2017-06-29' }, 'auto');
+      const period = retrieveComparePeriod({ start: '2017-06-01', end: '2017-06-29' }, 'auto');
       expect(period.start).to.equal('2017-05-03');
       expect(period.end).to.equal('2017-05-31');
     });
 
     it('should auto compare period for fitting the whole year be the year before', () => {
-      const period = datetimeUtils.retrieveComparePeriod({ start: '2016-01-01', end: '2016-12-31' }, 'auto');
+      const period = retrieveComparePeriod({ start: '2016-01-01', end: '2016-12-31' }, 'auto');
       expect(period.start).to.equal('2015-01-01');
       expect(period.end).to.equal('2015-12-31');
     });
 
     it('should 12_month_ago compare period for this month be the same number of days one year before', () => {
-      const period = datetimeUtils.retrieveComparePeriod('this_month', '12_months_ago');
+      const period = retrieveComparePeriod('this_month', '12_months_ago');
       expect(period.start).to.equal('2016-03-01');
       expect(period.end).to.equal('2016-03-19');
     });
 
     it('should 12_month_ago compare period for this quarter be the same number of days one year before', () => {
-      const period = datetimeUtils.retrieveComparePeriod('this_quarter', '12_months_ago');
+      const period = retrieveComparePeriod('this_quarter', '12_months_ago');
       expect(period.start).to.equal('2016-01-01');
       expect(period.end).to.equal('2016-03-19');
     });
 
     it('should return a custom comparison object', () => {
       const comparisonObj = { start: '2017-01-01', end: '2017-01-02' };
-      const period = datetimeUtils.retrieveComparePeriod('this_month', comparisonObj);
+      const period = retrieveComparePeriod('this_month', comparisonObj);
       expect(period).to.eql(comparisonObj);
     });
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
-import { addDays, addMonths, addWeeks, differenceInCalendarDays, differenceInDays, differenceInMonths, differenceInQuarters, differenceInWeeks, differenceInYears, eachDayOfInterval, endOfISOWeek, endOfMonth, endOfQuarter, endOfWeek, endOfYear, format, getDate, getDayOfYear, getDaysInMonth, getDaysInYear, getISODay, getMonth, getYear, isAfter, isBefore, isMonday, isSameDay, isSameMonth, isSunday, isToday, isValid, isWithinInterval, max, min, parseISO, setDate, setDay, setISODay, setMonth, startOfISOWeek, startOfMonth, startOfQuarter, startOfWeek, startOfYear, subDays, subMonths, subQuarters, subWeeks, subYears } from './helpers';
-import { calculateAutoCompare, migrateLegacyCompareMode, migrateLegacyPeriod, retrieveComparePeriod, retrievePeriod, retrievePeriodParams, toLegacyCompareMode, toLegacyPeriod } from './legacy';
-import { getAutoCompareRangeAndLabel, getCompareRange, getCustomPeriod, getLastPeriod, getPeriodParams, getRange, getThisPeriod, getTillYesterdayPeriod } from './main';
-import { formatDate } from './utils';
-
-export default {
-  // main
-  getRange,
-  getPeriodParams,
-  getCompareRange,
-  getAutoCompareRangeAndLabel,
-  getTillYesterdayPeriod,
-  getCustomPeriod,
-  getLastPeriod,
-  getThisPeriod,
-  // utils
-  formatDate,
-  // legacy
-  migrateLegacyPeriod,
-  migrateLegacyCompareMode,
-  toLegacyPeriod,
-  toLegacyCompareMode,
-  retrievePeriod,
-  retrievePeriodParams,
-  retrieveComparePeriod,
-  calculateAutoCompare,
-  // helpers
+export {
   addDays,
   addMonths,
   addWeeks,
@@ -74,4 +48,27 @@ export default {
   subQuarters,
   subWeeks,
   subYears,
-};
+} from './helpers';
+export {
+  calculateAutoCompare,
+  migrateLegacyCompareMode,
+  migrateLegacyPeriod,
+  retrieveComparePeriod,
+  retrievePeriod,
+  retrievePeriodParams,
+  toLegacyCompareMode,
+  toLegacyPeriod,
+} from './legacy';
+export {
+  getAutoCompareRangeAndLabel,
+  getCompareRange,
+  getCustomPeriod,
+  getLastPeriod,
+  getPeriodParams,
+  getRange,
+  getThisPeriod,
+  getTillYesterdayPeriod,
+} from './main';
+export {
+  formatDate,
+} from './utils';
