01f2533aceab1c24c66f8de2406041cd463652c5
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-10-08 08:40:33 +0200

    chore: fix release config

---
+++ release.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  branches: [
+    'master',
+    {
+      name: 'develop',
+      prerelease: 'beta',
+    },
+    {
+      name: 'alpha',
+      prerelease: 'alpha',
+    },
+  ],
+  repositoryUrl: 'https://github.com/nugit/datetime-utils.git',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'CHANGELOG.md',
+        ],
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      'semantic-release-slack-bot',
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        markdownReleaseNotes: true,
+      },
+    ],
+  ],
+};
