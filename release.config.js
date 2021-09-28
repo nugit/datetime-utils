f4ed427b16310e99321ef68cf63a44189e5a5553
Lim Ting Zhi <limtingzhi@gmail.com>
2021-09-28 16:00:13 +0800

    chore: remove @semantic-release/exec

--- release.config.js
+++ release.config.js
@@ -37,12 +37,6 @@ const mainConfig = {
       },
     ],
     [
-      '@semantic-release/exec',
-      {
-        publishCmd: 'git checkout -- package.json',
-      },
-    ],
-    [
       'semantic-release-slack-bot',
       {
         notifyOnSuccess: true,
@@ -78,12 +72,6 @@ const localConfig = {
       },
     ],
     '@semantic-release/npm',
-    [
-      '@semantic-release/exec',
-      {
-        publishCmd: 'git checkout -- package.json',
-      },
-    ],
   ],
 };
 
